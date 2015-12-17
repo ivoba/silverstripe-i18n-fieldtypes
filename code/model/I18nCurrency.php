ff7807ba61f29a3528ab7004d88d574922d5ffea
helpfulrobot <chris@silverstripe.com>
2015-12-18 09:52:32 +1300

    Converted to PSR-2

--- code/model/I18nCurrency.php
+++ code/model/I18nCurrency.php
@@ -5,68 +5,81 @@
  * @package modules: i18n_fieldtype
  * @author ivo.bathke
  */
-class I18nCurrency extends Currency {
+class I18nCurrency extends Currency
+{
 
-	protected static $decimalDelimiter = false;
-	protected static $thousandDelimiter = false;
+    protected static $decimalDelimiter = false;
+    protected static $thousandDelimiter = false;
 
-	public static function getCurrencySymbol() {
-		return self::$currencySymbol;
-	}
+    public static function getCurrencySymbol()
+    {
+        return self::$currencySymbol;
+    }
 
-	/**
-	 * formats value with Symbol and Delimiter
-	 * @see Currency::Nice()
-	 */
-	public function Nice() {
-		if(isset($this->value)){
-			self::init();
-			$val = self::$currencySymbol . number_format(abs($this->value), 2, self::$decimalDelimiter, self::$thousandDelimiter);
-			if($this->value < 0) return "($val)";
-			else return $val;
-		}
-	}
+    /**
+     * formats value with Symbol and Delimiter
+     * @see Currency::Nice()
+     */
+    public function Nice()
+    {
+        if (isset($this->value)) {
+            self::init();
+            $val = self::$currencySymbol . number_format(abs($this->value), 2, self::$decimalDelimiter, self::$thousandDelimiter);
+            if ($this->value < 0) {
+                return "($val)";
+            } else {
+                return $val;
+            }
+        }
+    }
 
-	/**
-	 * formats without symbol but with delimiter
-	 * @return string
-	 */
-	public function Pure() {
-		if(isset($this->value)){
-			self::init();
-			return number_format(abs($this->value), 2, self::$decimalDelimiter, self::$thousandDelimiter);
-		}
-	}
+    /**
+     * formats without symbol but with delimiter
+     * @return string
+     */
+    public function Pure()
+    {
+        if (isset($this->value)) {
+            self::init();
+            return number_format(abs($this->value), 2, self::$decimalDelimiter, self::$thousandDelimiter);
+        }
+    }
 
-	/**
-	 * return unformatted value
-	 */
-	public function Raw() {
-		$this->getValue();
-	}
+    /**
+     * return unformatted value
+     */
+    public function Raw()
+    {
+        $this->getValue();
+    }
 
-	public static function setDecimalDelimiter($value) {
-		self::$decimalDelimiter = $value;
-	}
+    public static function setDecimalDelimiter($value)
+    {
+        self::$decimalDelimiter = $value;
+    }
 
-	public static function setThousandDelimiter($value) {
-		self::$thousandDelimiter = $value;
-	}
+    public static function setThousandDelimiter($value)
+    {
+        self::$thousandDelimiter = $value;
+    }
 
-	public static function getDecimalDelimiter() {
-		return self::$decimalDelimiter;
-	}
+    public static function getDecimalDelimiter()
+    {
+        return self::$decimalDelimiter;
+    }
 
-	public static function getThousandDelimiter() {
-		return self::$thousandDelimiter;
-	}
+    public static function getThousandDelimiter()
+    {
+        return self::$thousandDelimiter;
+    }
 
-	private static function init() {
-		if(self::$decimalDelimiter === false){
-			self::$decimalDelimiter = _t('CURRENCY.DECIMALDELIMITER','.');
-		}
-		if(self::$thousandDelimiter === false){
-			self::$thousandDelimiter = _t('CURRENCY.THOUSANDDELIMITER','');
-		}
-	}
+    private static function init()
+    {
+        if (self::$decimalDelimiter === false) {
+            self::$decimalDelimiter = _t('CURRENCY.DECIMALDELIMITER', '.');
+        }
+        if (self::$thousandDelimiter === false) {
+            self::$thousandDelimiter = _t('CURRENCY.THOUSANDDELIMITER', '');
+        }
+    }
 }
--- code/model/I18nMoney.php
+++ code/model/I18nMoney.php
@@ -5,65 +5,77 @@
  * @package modules: i18n_fieldtypes
  * @author ivo.bathke
  */
-class I18nMoney extends Money{
+class I18nMoney extends Money
+{
 
-	/**
-	 * possible options
-	 * @see http://framework.zend.com/manual/en/zend.currency.options.html
-	 'currency',
-	 'display',
-	 'format',
-	 'locale',
-	 'name',
-	 'position',
-	 'precision',
-	 'script',
-	 'service',
-	 'symbol',
-	 'value'
-	 
-	 NOTE locale is not supported, though the doku says so
-	 TODO file a bug @Zend & @sapphire
-	 */
-	protected static $options = array();
-	
-	/**
-	 * @return string
-	 */
-	public function Nice($options = array()) {
-		$options = $this->initOptions($options);
-		$amount = $this->getAmount();
-		if(!isset($options['display'])) $options['display'] = Zend_Currency::USE_SYMBOL;
-		if(!isset($options['currency'])) $options['currency'] = $this->getCurrency();
-		if(!isset($options['symbol'])) {
-			$options['symbol'] = $this->currencyLib->getSymbol($this->getCurrency(), $this->getLocale());
-		}
-		return (is_numeric($amount)) ? $this->currencyLib->toCurrency($amount, $options) : '';
-	}
+    /**
+     * possible options
+     * @see http://framework.zend.com/manual/en/zend.currency.options.html
+     'currency',
+     'display',
+     'format',
+     'locale',
+     'name',
+     'position',
+     'precision',
+     'script',
+     'service',
+     'symbol',
+     'value'
+     
+     NOTE locale is not supported, though the doku says so
+     TODO file a bug @Zend & @sapphire
+     */
+    protected static $options = array();
+    
+    /**
+     * @return string
+     */
+    public function Nice($options = array())
+    {
+        $options = $this->initOptions($options);
+        $amount = $this->getAmount();
+        if (!isset($options['display'])) {
+            $options['display'] = Zend_Currency::USE_SYMBOL;
+        }
+        if (!isset($options['currency'])) {
+            $options['currency'] = $this->getCurrency();
+        }
+        if (!isset($options['symbol'])) {
+            $options['symbol'] = $this->currencyLib->getSymbol($this->getCurrency(), $this->getLocale());
+        }
+        return (is_numeric($amount)) ? $this->currencyLib->toCurrency($amount, $options) : '';
+    }
 
-	/**
-	 * @return string
-	 */
-	public function Pure($options = array()) {
-		$options = $this->initOptions($options);
-		if(isset($options['display'])) $options['display'] = Zend_Currency::NO_SYMBOL;
-		$amount = $this->getAmount();
-		return (is_numeric($amount)) ? $this->currencyLib->toCurrency($amount, $options) : '';
-	}
-	
-	/**
-	 * @return decimal
-	 */
-	public function Raw() {
-		return $this->getAmount();
-	}
+    /**
+     * @return string
+     */
+    public function Pure($options = array())
+    {
+        $options = $this->initOptions($options);
+        if (isset($options['display'])) {
+            $options['display'] = Zend_Currency::NO_SYMBOL;
+        }
+        $amount = $this->getAmount();
+        return (is_numeric($amount)) ? $this->currencyLib->toCurrency($amount, $options) : '';
+    }
+    
+    /**
+     * @return decimal
+     */
+    public function Raw()
+    {
+        return $this->getAmount();
+    }
 
-	public static function setOptions($options) {
-		self::$options = $options;
-	}
-	
-	private function initOptions($options) {
-		$this->currencyLib->setLocale(i18n::get_locale());
-		return array_merge($options, self::$options);
-	}
+    public static function setOptions($options)
+    {
+        self::$options = $options;
+    }
+    
+    private function initOptions($options)
+    {
+        $this->currencyLib->setLocale(i18n::get_locale());
+        return array_merge($options, self::$options);
+    }
 }
